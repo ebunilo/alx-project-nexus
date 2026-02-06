57366abfcd780d41895c6a7b5d3d68b60dec4045
Ebunilo Igwilo <ebuniloigwilo@gmail.com>
2026-02-06 15:46:03 +0100

    tidy up host and container ports for redis and celery

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -188,12 +188,10 @@ REDOC_SETTINGS = {
 CHAPA_SECRET_KEY=env('CHAPA_SECRET_KEY', default='CHASECK_TEST-jqv5mCEnhgE6PNdTZoBN1zBVHPqTfh')
 
 # Celery settings
-CELERY_BROKER_URL = env('CELERY_BROKER_URL', default='redis://redis:6380/0')
+CELERY_BROKER_URL = env('CELERY_BROKER_URL', default=f'redis://redis:{env("REDIS_PORT", default=6379)}/0')
+CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND', default=f'redis://redis:{env("REDIS_PORT", default=6379)}/0')
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
-
-# Celery results backend
-CELERY_RESULT_BACKEND = env('CELERY_RESULT_BACKEND', default='django-db')
 
 # Email settings
 EMAIL_BACKEND = env('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
